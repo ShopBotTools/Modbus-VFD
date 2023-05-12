8a595c4d2033ab8d39cb4e419e86c7f981acb982
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2023-05-12 10:46:10 -0400

    First commit

--- Modbus_Settings.py
+++ Modbus_Settings.py
@@ -20,8 +20,8 @@ Thanks For Watching!!!
 
 ## General Modbus Settings
 
-mb_address = 10							# Station Address
-USB_port = "/dev/ttyUSB0"				# Location of USB to RS485 converter
+mb_address = 1							# Station Address
+USB_port = "COM3"				# Location of USB to RS485 converter
 baudrate = 9600							# BaudRate
 bytesize = 8							# Number of data bits to be requested
 stopbits = 1							# Number of stop bits
