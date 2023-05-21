df0ec4ce83a97d31b161795f81dd9b3f0c1d04b6
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2023-05-21 14:05:22 -0400

    Grid layed out

--- gui.py
+++ gui.py
@@ -1,34 +1,62 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import font
 
-root = Tk()  # create a root widget
+root = tk.Tk()  # create a root widget
+
+FONT_SIZE = font.Font(size=15)
+WINDOW_BACKGROUND = "white"
+FONT_BACKGROUND = "white"
+X_PADDING = 10
+
 root.title("Spindle Control")
-root.config(background="white")
+root.config(background=WINDOW_BACKGROUND)
 root.minsize(100, 100)  # width, height
 root.maxsize(1000, 1000)
 root.geometry("700x150+50+50")  # width x height + x + y
 
-# print(f"Current        :  {current/10}A")
+# print(f"Current        :  {current/X_PADDING}A")
 # print(f"Frequency      :  {float(hirz/100)}Hz")
 # print(f"Output Voltage :  {volt}V")
 # print(f"DC Bus Voltage :  {bus}V")
-# print(f"Total Power    :  {power/10}kW")
+# print(f"Total Power    :  {power/X_PADDING}kW")
 # print(f"Operation Code :  {process}")
 
-# display_info = Label(root, text="", bg="white")
-# display_info.grid(row=0, column=1, columnspan=2)
-current_frame = Frame(root, bg="white")
-current_frame.pack(side="left")
-current_text = Label(current_frame, text="Current", bg="white", font=("Font", 15)).pack(side="left", padx=15)
-frequency_text = Label(current_frame, text="Frequency", bg="white", font=("Font", 15)).pack(side="left", padx=15)
+#Column 1, Keys
+current = tk.Label(root, text="Current", bg=FONT_BACKGROUND, font=FONT_SIZE, anchor="w")
+current.grid(row=1, column=1, padx=X_PADDING)
 
-# text2.pack(side="left")
-# text2 = Label(root, text="Output Voltage")
-# text2.pack(side="left")
-# text2 = Label(root, text="DC Bus Voltage")
-# text2.pack(side="left")
-# text2 = Label(root, text="Total Power")
-# text2.pack(side="left")
-# text2 = Label(root, text="Operation Code")
-# text2.pack(side="left")
+frequency = tk.Label(root, text="Frequency", bg=FONT_BACKGROUND, font=FONT_SIZE)
+frequency.grid(row=2, column=1, padx=X_PADDING)
+
+output_voltage = tk.Label(root, text="Output Voltage", bg=FONT_BACKGROUND, font=FONT_SIZE)
+output_voltage.grid(row=3, column=1, padx=X_PADDING)
+
+dc_voltage = tk.Label(root, text="DC Bus Voltage", bg=FONT_BACKGROUND, font=FONT_SIZE)
+dc_voltage.grid(row=4, column=1, padx=X_PADDING)
+
+total_power = tk.Label(root, text="Total Power", bg=FONT_BACKGROUND, font=FONT_SIZE)
+total_power.grid(row=5, column=1, padx=X_PADDING)
+
+operation_code = tk.Label(root, text="Operation Code", bg=FONT_BACKGROUND, font=FONT_SIZE)
+operation_code.grid(row=6, column=1, padx=X_PADDING)
+
+#Column 2, Values
+current = tk.Label(root, text="Current Value", bg=FONT_BACKGROUND, font=FONT_SIZE, anchor="w")
+current.grid(row=1, column=2, padx=X_PADDING)
+
+frequency = tk.Label(root, text="Frequency Value", bg=FONT_BACKGROUND, font=FONT_SIZE)
+frequency.grid(row=2, column=2, padx=X_PADDING)
+
+output_voltage = tk.Label(root, text="Output Voltage Value", bg=FONT_BACKGROUND, font=FONT_SIZE)
+output_voltage.grid(row=3, column=2, padx=X_PADDING)
+
+dc_voltage = tk.Label(root, text="DC Bus Voltage Value", bg=FONT_BACKGROUND, font=FONT_SIZE)
+dc_voltage.grid(row=4, column=2, padx=X_PADDING)
+
+total_power = tk.Label(root, text="Total Power Value", bg=FONT_BACKGROUND, font=FONT_SIZE)
+total_power.grid(row=5, column=2, padx=X_PADDING)
+
+operation_code = tk.Label(root, text="Operation Code Value", bg=FONT_BACKGROUND, font=FONT_SIZE)
+operation_code.grid(row=6, column=2, padx=X_PADDING)
 root.iconbitmap("rpm.ico")
 root.mainloop()
