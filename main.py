71e1e621848c82c8f17fce71bd071af2a0cef873
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2023-05-24 13:11:08 -0400

    Some cleanup

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from controller import VFDController
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--speed", type=int,  help = "Change the spindle speed, 60-120 currently")
+parser.add_argument("-s", "--speed", type=int,  help = "Port must be supplied with -p. Change the spindle speed, 60-120 currently")
 parser.add_argument("-p", "--port", type=int, help = "Dictates the com port, enter 3 for COM3, 4 for COM4, etc...")
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    if args.port:
+    if not args:
+        controller = VFDController(3)
+        controller.start()
+    elif args.port and args.speed:
         port_string = str(args.port)
         print(f"Port: {port_string}")
         controller = VFDController(port_string)
@@ -15,8 +18,3 @@ if __name__ == "__main__":
         if args.speed:
             print(f"Speed: {args.speed}")
             controller.set_current(args.speed)
-        if not args.speed:
-            print("must specify port and speed")
-    else:
-        controller = VFDController(3)
-        controller.start()
