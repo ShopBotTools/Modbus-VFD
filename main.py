ddd77c5abb87536c8bc34f4aa46127b09ea24730
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2023-05-25 01:26:53 -0400

    Command line arguments working as well.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import argparse
 from controller import VFDController
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--speed", type=int,  help = "Port must be supplied with -p. Change the spindle speed, 60-120 currently")
+parser.add_argument("-s", "--speed", type=int,  help = "Port must be supplied with -p. Change the spindle speed, 3600-7200 currently")
+parser.add_argument("-f", "--frequency", type=int,  help = "Port must be supplied with -p. Change the frequency, 60-120 currently")
 parser.add_argument("-p", "--port", type=int, help = "Dictates the com port, enter 3 for COM3, 4 for COM4, etc...")
 args = parser.parse_args()
 
@@ -10,12 +11,18 @@ if __name__ == "__main__":
     try:
         if args.port and args.speed:
             port_string = str(args.port)
-            print(f"Port: {port_string}")
             controller = VFDController(port_string)
             controller.connect(f"COM{port_string}")
-            if args.speed:
-                print(f"Speed: {args.speed}")
-                controller.set_current(args.speed)
+            # Convert speed to frequency to pass to set_spindle
+            int_speed = int(args.speed)
+            controller.set_spindle(int_speed / 60)
+        elif args.port and args.frequency:
+            port_string = str(args.port)
+            controller = VFDController(port_string)
+            controller.connect(f"COM{port_string}")
+            # Convert speed to frequency to pass to set_spindle
+            int_frequency = int(args.frequency)
+            controller.set_frequency(int_frequency)
         else:
             controller = VFDController(3)
             controller.start()
--- user_inputs.py
+++ user_inputs.py
@@ -1,9 +1,9 @@
 ## Create function for prompting the user to input the desired RPM and returns the value to be given to the VFD frequency address
 ## Returns the string "NaN" if the user input is not a number
 ## Returns the string "OL" if the user input is outside the limits of 0-60Hz
-def set_user_spindle(input):
+def set_user_spindle(user_input):
     try:
-        speed_int = int(float(input)*10)
+        speed_int = int(float(user_input)*10)
     except TypeError:
         return "Invalid input"
     except ValueError:
@@ -17,9 +17,9 @@ def set_user_spindle(input):
         else:
             return "NaN"
 
-def set_user_frequency(input):
+def set_user_frequency(user_input):
     try:
-        speed_int = int(float(input)*10)
+        speed_int = int(float(user_input)*10)
     except TypeError:
         return "Invalid input"
     except ValueError:
