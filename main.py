97f2206f1b51ec05d767e6c27807d42b44c3a8a0
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2023-05-27 18:08:01 -0400

    Slight changes

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ parser.add_argument("-f", "--frequency", type=int,  help = "Change the frequency
 args = parser.parse_args()
 
 if __name__ == "__main__":
-        controller = VFDController(MB.COM_PORT)
-        if args.speed:
-            # Convert speed to frequency to pass to set_spindle
-            controller.connect(MB.COM_PORT)
-            int_speed = int(args.speed)
-            controller.set_spindle(int_speed / 60)
-        if args.frequency:
-            controller.connect(MB.COM_PORT)
-            int_frequency = int(args.frequency)
-            controller.set_frequency(int_frequency)
-        if args.speed is None and args.frequency is None:
-            try:
-                controller.start()
-            except KeyboardInterrupt:
-                print("CTRL+C detected, exiting")
+    controller = VFDController(MB.COM_PORT)
+    if args.speed:
+        # Convert speed to frequency to pass to set_spindle
+        controller.connect(MB.COM_PORT)
+        int_speed = int(args.speed)
+        controller.set_spindle(int_speed / 60)
+    if args.frequency:
+        controller.connect(MB.COM_PORT)
+        int_frequency = int(args.frequency)
+        controller.set_frequency(int_frequency)
+    if args.speed is None and args.frequency is None:
+        try:
+            controller.start()
+        except KeyboardInterrupt:
+            print("CTRL+C detected, exiting")
