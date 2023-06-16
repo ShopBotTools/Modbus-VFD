58e974f976000892ac3b4147dd2f4ce83ce1b374
kyle <kyle.kingsbury@snhu.edu>
2023-06-16 11:58:44 -0400

    First try

--- modbus_settings.py
+++ modbus_settings.py
@@ -3,47 +3,10 @@
 import serial
 import serial.tools.list_ports
 import re
-from pymodbus.client.sync import ModbusSerialClient
-
-def is_modbus_vfd_controller(com_port):
-    # Create a Modbus client
-    client = ModbusSerialClient(method='rtu', port=com_port, baudrate=9600)
-
-    try:
-        # Connect to the Modbus device
-        if client.connect():
-            # Send a simple read request to address 0
-            response = client.read_holding_registers(0, 1, unit=1)
-            if response.isError():
-                # Modbus read request was unsuccessful
-                return False
-            else:
-                # Modbus read request was successful
-                return True
-        else:
-            # Failed to connect to the Modbus device
-            return False
-    finally:
-        # Close the Modbus client
-        client.close()
-
-def get_com_port():
-    # Get a list of all available ports
-    available_ports = list(serial.tools.list_ports.comports())
-
-    pattern = r"ShopBot Controller \((COM\d+)\)"
-    for port in available_ports:
-        match = re.match(pattern, port.description)
-        if match:
-            com_port = match.group(1)
-            # Check if it is the correct port by sending a modbus read request
-            if is_modbus_vfd_controller(com_port):
-                return com_port
-
-    return None
+import minimalmodbus
+import threading
 
 MB_ADDRESS = 3                          # Station Address
-COM_PORT = get_com_port()
 BAUDRATE = 9600                         # BAUDRATE
 BYTESIZE = 8                            # Number of data bits to be requested
 STOPBITS = 1                            # Number of stop bits
@@ -71,3 +34,59 @@ WRITE_SINGLE_REGISTER = 6
 
 ## Read Settings
 READ_LENGTH = 6                # Number of adresses to read when Polling the VFD for data
+
+lock = threading.Lock()
+
+def get_com_port():
+    # Get a list of all available ports
+    available_ports = list(serial.tools.list_ports.comports())
+
+    pattern = r"ShopBot Controller \((COM\d+)\)"
+    for port in available_ports:
+        match = re.match(pattern, port.description)
+        if match:
+            com_port = match.group(1)
+            # Check if it is the correct port by sending a modbus read request
+            if is_modbus_vfd_controller(com_port):
+                return com_port
+
+    return None
+
+def is_modbus_vfd_controller(com_port):
+    try:
+        vfd = minimalmodbus.Instrument(com_port, MB_ADDRESS)
+        vfd.mode = minimalmodbus.MODE_RTU
+        vfd.serial.parity = minimalmodbus.serial.PARITY_NONE
+        vfd.serial.baudrate = BAUDRATE
+        vfd.serial.bytesize = BYTESIZE
+        vfd.serial.stopbits = STOPBITS
+        vfd.serial.timeout = TIMEOUT
+        vfd.clear_buffers_before_each_transaction = CLEAR_BUFFERS_BEFORE_CALL
+        vfd.close_port_after_each_call = CLEAR_BUFFERS_AFTER_CALL
+        vfd.debug = DEBUG
+    except minimalmodbus.ModbusException as e:
+        # Handle modbus communication error
+        print("Modbus Exception:", e)
+    except PermissionError as e:
+        # Handle permission error
+        print("Permission Error:", e)
+    except Exception as e:
+        # Handle other exceptions
+        print("Exception:", e)
+    with lock:
+        try:
+            # Read data from the device
+            data = vfd.read_registers(READ_FREQUENCY, READ_LENGTH, READ_REGISTER)
+            return data
+
+        except minimalmodbus.ModbusException as e:
+            # Handle modbus communication error
+            print("Modbus Exception:", e)
+        except PermissionError as e:
+            # Handle permission error
+            print("Permission Error:", e)
+        except Exception as e:
+            # Handle other exceptions
+            print("Exception:", e)
+
+COM_PORT = get_com_port()
